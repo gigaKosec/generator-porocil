e59cff0e6eddfb4d268c24d5cbb17d72ddd6a54b
gigaKosec <giga.kosec@gmail.com>
2020-12-30 18:38:29 +0100

    - spremenil način izbire zadnjega dne v tednu - dodelal izpise podatkov o dnevu (locale = slo)

--- src/components/DneviForm.vue
+++ src/components/DneviForm.vue
@@ -5,17 +5,30 @@
         <b-row>
           <b-col>
             <b-row>
-              <b-form-radio-group id="type-of-report" v-model="typeOfReport" buttons>
-                <b-form-radio value="weekly" button-variant="outline-primary"> teden </b-form-radio>
-                <b-form-radio value="monthly" button-variant="outline-primary"> mesec </b-form-radio>
-              </b-form-radio-group>
+              <b-input-group prepend="Poročilo">
+                <b-form-radio-group
+                  id="type-of-report"
+                  v-model="typeOfReport"
+                  buttons
+                >
+                  <b-form-radio value="weekly" button-variant="outline-primary">
+                    tedensko
+                  </b-form-radio>
+                  <b-form-radio
+                    value="monthly"
+                    button-variant="outline-success"
+                  >
+                    mesečno
+                  </b-form-radio>
+                </b-form-radio-group>
+              </b-input-group>
             </b-row>
 
             <b-row>
               <b-input-group
-                prepend="ki vsebuje"
+                prepend="Izberi"
                 style="width: 20rem"
-                title="Izberi katerikoli dan iz tedna"
+                title="Izberi katerikoli dan iz tedna/meseca"
               >
                 <b-form-datepicker
                   id="selected-date"
@@ -32,6 +45,7 @@
                   style="width: 170px"
                   @input="renderNewReport"
                 ></b-form-datepicker>
+                <!-- <div>OD: {{formatISO(firstOfSelectedDays,{<<})}} DO: {{ISOdate(lastOfSelectedDays)}} </div> -->
               </b-input-group>
               <!-- <b-button>pošlji</b-button> -->
             </b-row>
@@ -50,12 +64,17 @@
             ></b-col
           >
           <b-col>
-            <b-row><b-checkbox id="show-output" v-model="showOutput">
-              prikaži output</b-checkbox
-            >
-            <b-button size="sm" variant="danger" v-if="showOutput" @click="clearLocalStorage"
-              >ZBRIŠI LOCAL STORAGE</b-button
-            >
+            <b-row
+              ><b-checkbox id="show-output" v-model="showOutput" switch>
+                prikaži output</b-checkbox
+              >
+              <b-button
+                size="sm"
+                variant="danger"
+                v-if="showOutput"
+                @click="clearLocalStorage"
+                >ZBRIŠI LOCAL STORAGE</b-button
+              >
             </b-row>
           </b-col>
         </b-row>
@@ -68,36 +87,44 @@
           v-bind:key="index"
         >
           <!-- 1. STOLPEC: PODATKI O DNEVU-->
-          <b-col cols="2" style="min-width: 160px">
+          <b-col cols="2" style="min-width: 170px">
             <!-- ime dneva v tednu -->
-            <b-row>  
-              <h2 class="day-name">{{dayOfWeek(day.datum)}}</h2>
+            <b-row>
+              <h2 class="day-name">{{ dayOfWeek(day.datum) }}</h2>
             </b-row>
             <!-- datum -->
             <b-row>
-              <div class=date>{{ ISOdate(new Date(day.datum)) }}</div>
+              <div class="date">{{new Date(day.datum).toLocaleDateString("en-UK",{day:"2-digit", month:"short"})}}</div>
             </b-row>
             <b-row class="daily-location">
               <!-- lokacija -->
-              <b-form-radio-group v-model="day.lokacijaDela" inline="true" buttons>
-                <b-form-radio value="doma" button-variant="outline-secondary" >doma</b-form-radio>
-                <b-form-radio value="služba" button-variant="outline-secondary">služba </b-form-radio>
+              <b-form-radio-group
+                v-model="day.lokacijaDela"
+                inline="true"
+                buttons
+              >
+                <b-form-radio value="doma" button-variant="outline-secondary"
+                  >od doma</b-form-radio
+                >
+                <b-form-radio value="služba" button-variant="outline-success"
+                  >v službi
+                </b-form-radio>
               </b-form-radio-group>
             </b-row>
             <b-row class="daily-hours">
               <!-- št ur -->
-              <label>št. ur:</label>
-              <b-form-input
-                type="number"
-                v-model="day.stUr"
-                style="width: 60px"
-              ></b-form-input>
+              <b-input-group prepend="št. ur">
+                <b-form-input
+                  type="number"
+                  v-model="day.stUr"
+                ></b-form-input>
+              </b-input-group>
             </b-row>
           </b-col>
           <!-- 2. STOLPEC: opis dela -->
           <b-col class="daily-work-description">
             <b-textarea
-              rows="5"
+              rows="4"
               :value="day.opisDela"
               v-model="day.opisDela"
             ></b-textarea>
@@ -120,18 +147,14 @@
 <script>
 import { setDay, eachDayOfInterval, parseJSON, parseISO } from "date-fns";
 import * as Fdate from "date-fns";
-//import {loadDays,storeDays, days} from '@/services/storeToLocalStorage';
-/* import {
-  loadAllReports,
-  storeReports,
-  //SingleDayReport,
-} from "@/services/storageAlter.js"; */
+import { sl } from 'date-fns/locale';
+
 
 export default {
   data() {
     return {
       typeOfReport: "weekly",
-      chosenDate: null,  //new Date(), PAZI - moraš še odrezat ure itd.
+      chosenDate: null, //new Date(), PAZI - moraš še odrezat ure itd.
       showOutput: false,
       datesChosen: [],
       reportsStored: {},
@@ -139,41 +162,43 @@ export default {
     };
   },
   computed: {
-    firstDayOfWeekDate: function () {
+    firstOfSelectedDays: function () {
       return setDay(this.chosenDate, 1);
     },
-    lastDayOfWeekDate: function () {
-      /* console.log("last date of week =", this.addDays(this.firstDayOfWeekDate,5)) */
-      return this.addDays(this.firstDayOfWeekDate, 4);
+    lastOfSelectedDays: function () {
+      /* console.log("last date of week =", this.addDays(this.firstOfSelectedDays,5)) */
+      return setDay(this.chosenDate, 5);
     },
-  }, 
+  },
 
   mounted() {
     this.reportsStored = this.loadAllReports();
-    this.chosenDate = new Date()
-    this.chosenDate.setHours(0,0,0,0)
-    this.renderNewReport()
+    this.chosenDate = new Date();
+    this.chosenDate.setHours(0, 0, 0, 0);
+    this.renderNewReport();
     //this.generateChosenDays()
-    
   },
 
   methods: {
-    renderNewReport (){
-      this.storeReports()
-      this.generateChosenDays()
-      this.getMultipleDaysReportsForDatesChosen()
+    renderNewReport() {
+      this.storeReports();
+      this.generateChosenDays();
+      this.getMultipleDaysReportsForDatesChosen();
     },
-    
-    generateChosenDays () {
-      console.log("THE date chosen =", this.chosenDate)
+
+    generateChosenDays() {
+      console.log("THE date chosen =", this.chosenDate);
       let result = eachDayOfInterval({
-        start: this.firstDayOfWeekDate,
-        end: this.lastDayOfWeekDate,
+        start: this.firstOfSelectedDays,
+        end: this.lastOfSelectedDays,
       });
 
       this.datesChosen = result;
       console.log("dates chosen = ", this.datesChosen);
-      console.log("Multiple days reports pred generiranjem:", this.multipleDaysReports)
+      console.log(
+        "Multiple days reports pred generiranjem:",
+        this.multipleDaysReports
+      );
     },
     //loadDays
     //loadAllReports,
@@ -183,38 +208,52 @@ export default {
 
     getMultipleDaysReportsForDatesChosen() {
       class SingleDayReport {
-        constructor(datum = null, lokacijaDela = "doma", stUr = 8, opisDela = "") {
+        constructor(
+          datum = null,
+          lokacijaDela = "doma",
+          stUr = 8,
+          opisDela = ""
+        ) {
           this.datum = datum;
           this.lokacijaDela = lokacijaDela;
           this.stUr = stUr;
           this.opisDela = opisDela;
         }
       }
-      this.multipleDaysReports = {}
+      this.multipleDaysReports = {};
       for (let date of this.datesChosen) {
-        const dateAsKey=this.ISOdate(date)
-        console.log("type od prvega datuma =", typeof dateAsKey)
-        console.log("type od drugega datuma = ", typeof (Object.keys(this.reportsStored)[0]) )
-        console.log("new key for date =",dateAsKey)
-        
-        if ((Object.keys(this.reportsStored)).includes(dateAsKey)) {
+        const dateAsKey = this.ISOdate(date);
+        console.log("type od prvega datuma =", typeof dateAsKey);
+        console.log(
+          "type od drugega datuma = ",
+          typeof Object.keys(this.reportsStored)[0]
+        );
+        console.log("new key for date =", dateAsKey);
+
+        if (Object.keys(this.reportsStored).includes(dateAsKey)) {
           console.log("datum najden v reportsStore");
           this.multipleDaysReports[dateAsKey] = this.reportsStored[dateAsKey];
         } else {
-          console.log("NISEM našel datuma", dateAsKey, "v", Object.keys(this.reportsStored));
+          console.log(
+            "NISEM našel datuma",
+            dateAsKey,
+            "v",
+            Object.keys(this.reportsStored)
+          );
           let dailyInput = new SingleDayReport(date);
-          
-          console.log("generiram nov defaultni dnevni vnos")
-          console.log("daily Input =", dailyInput)
+
+          console.log("generiram nov defaultni dnevni vnos");
+          console.log("daily Input =", dailyInput);
           this.multipleDaysReports[dateAsKey] = dailyInput;
-          
         }
       }
-      console.log("MultipleDaysReports po generiranju = ", this.multipleDaysReports)
+      console.log(
+        "MultipleDaysReports po generiranju = ",
+        this.multipleDaysReports
+      );
     },
 
     // STORING METHODS
-    
 
     clearLocalStorage() {
       if (confirm("Ali res želiš izbrisati vse vnose?")) {
@@ -223,27 +262,27 @@ export default {
       }
     },
     storeReports() {
-      console.log("SHRANJUJEM V LOCAL STORE")
-      
-      for (let [key,value] of Object.entries(this.multipleDaysReports)) {
-        console.log("  key =", key)
-        console.log("value =",value)
-        this.reportsStored[key] = value
-        console.log("reports stored =",this.reportsStored)
+      console.log("SHRANJUJEM V LOCAL STORE");
+
+      for (let [key, value] of Object.entries(this.multipleDaysReports)) {
+        console.log("  key =", key);
+        console.log("value =", value);
+        this.reportsStored[key] = value;
+        console.log("reports stored =", this.reportsStored);
       }
-      console.log("KONČNI reports stored",this.reportsStored)
-      localStorage.setItem("reportsOfWork", JSON.stringify(this.reportsStored))
-      
+      console.log("KONČNI reports stored", this.reportsStored);
+      localStorage.setItem("reportsOfWork", JSON.stringify(this.reportsStored));
     },
 
     loadAllReports() {
-      if (localStorage.getItem("reportsOfWork") === null){
-        console.log("ni našel ničesar v local storage")
-        return {}
-      }
-      else {
+      if (localStorage.getItem("reportsOfWork") === null) {
+        console.log("ni našel ničesar v local storage");
+        return {};
+      } else {
         //let localReportsStored = {}
-        let tempReportsStored =  JSON.parse(localStorage.getItem("reportsOfWork"));
+        let tempReportsStored = JSON.parse(
+          localStorage.getItem("reportsOfWork")
+        );
         //let prviKey = Object.keys(tempReportsStored)[0]
         //console.log("v local storageu našel: ", tempReportsStored)
         //console.log("keys = ", Object.keys(tempReportsStored))
@@ -266,8 +305,8 @@ export default {
     },
 
     // DATE RELATED METHODS
-    ISOdate(date){
-      return Fdate.formatISO(date,{representation: "date"})
+    ISOdate(date) {
+      return Fdate.formatISO(date, { representation: "date" });
     },
     dateDisabled(ymd, date) {
       // Disable weekends (Sunday = `0`, Saturday = `6`)
@@ -280,11 +319,11 @@ export default {
       result.setDate(result.getDate() + days);
       return result;
     },
-    dayOfWeek (dateString) {
-      return Fdate.format(new Date(dateString),'eee')
+    dayOfWeek(dateString) {
+      return Fdate.format(new Date(dateString), "EEEE", {locale: sl});
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
